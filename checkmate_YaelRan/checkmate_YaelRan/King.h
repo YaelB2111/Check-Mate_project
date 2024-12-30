482547e046c410b22ea5b0181759d485f25647f4
YaelB2111 <yaelback08@gmail.com>
2024-12-30 16:15:09 +0200

    create class king

---
+++ checkmate_YaelRan/checkmate_YaelRan/King.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Piece.h"
+#include <cctype>
+#include <iostream>
+#include <math.h>
+
+class King : public Piece
+{
+public:
+	King(char name);
+	~King();
+	virtual bool IsMoveLegal(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[], int& result, bool whitePlays) override;
+};
+
