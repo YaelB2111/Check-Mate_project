1cfc3d2c03db6d391d4d76e463af2cfc22c2d270
YaelB2111 <yaelback08@gmail.com>
2025-01-02 09:08:08 +0200

    created class pawn

---
+++ checkmate_YaelRan/checkmate_YaelRan/Pawn.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Piece.h"
+#include <cctype>
+#include <iostream>
+
+class Pawn : public Piece
+{
+	Pawn(char name);
+	~Pawn();
+	virtual bool IsMoveLegal(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[], int& result, bool whitePlays) override;
+};
+
