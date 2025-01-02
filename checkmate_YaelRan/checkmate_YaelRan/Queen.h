71db7fa96b6c7329c4246a6170f6c5158710cfd7
YaelB2111 <yaelback08@gmail.com>
2025-01-02 08:20:19 +0200

    created class queen

---
+++ checkmate_YaelRan/checkmate_YaelRan/Queen.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Piece.h"
+#include <cctype>
+#include <iostream>
+
+class Queen : public Piece
+{
+	Queen(char name);
+	~Queen();
+
+	virtual bool IsMoveLegal(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[], int& result, bool whitePlays) override;
+	//rook based funca
+	bool IsLegalForward(const int destY, const int srcX, const int srcY, const Piece* board[]);
+	bool IsLegalBackward(const int destY, const int srcX, const int srcY, const Piece** board);
+	bool IsLegalRight(const int destX, const int srcX, const int srcY, const Piece** board);
+	bool IsLegalLeft(const int destX, const int srcX, const int srcY, const Piece** board);
+
+	//bishop based funcs
+	bool MoveRightTop(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[]) const;
+	bool MoveRightBot(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[]) const;
+	bool MoveLeftTop(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[]) const;
+	bool MoveLeftBot(const int destX, const int destY, const int srcX, const int srcY, const Piece* board[]) const;
+};
+
