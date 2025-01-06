aa2ec7ab4dc7f4a32cc6f01dd4690079a11f62af
YaelB2111 <yaelback08@gmail.com>
2025-01-06 09:47:10 +0200

    save changes

--- checkmate_YaelRan/checkmate_YaelRan/Piece.h
+++ checkmate_YaelRan/checkmate_YaelRan/Piece.h
@@ -27,6 +27,7 @@ public:
 	bool IsSelfCheck(const int srcX, const int srcY, const Piece*** board, const bool whitePlays);
 	void Eat(const int destX, const int destY, const int srcX, int srcY, Piece*** board);
 	virtual bool IsMoveLegal(const int destX, const int destY, const int srcX, const int srcY, const Piece*** board, int& result, bool whitePlays) = 0;
+	
 private:
 	char _name;
 
