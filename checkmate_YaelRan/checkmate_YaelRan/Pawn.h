6d071953d4d0bc6016e34d8c738c4e6416d38c68
YaelB2111 <yaelback08@gmail.com>
2025-01-07 11:11:28 +0200

    fixed queen

--- checkmate_YaelRan/checkmate_YaelRan/Pawn.h
+++ checkmate_YaelRan/checkmate_YaelRan/Pawn.h
@@ -10,7 +10,7 @@ public:
 	Pawn(char name);
 	~Pawn();
 	virtual bool IsMoveLegal(const int destX, const int destY, const int srcX, const int srcY, const Piece** board[], int& result, bool whitePlays) override;
-
+	void SetMoved();
 private:
 	bool _moved;
 };
